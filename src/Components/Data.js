30366c09b3661cb9bc335183589e837713628231
deena <deenadinn007@gmail.com>
2022-03-12 17:46:29 +0530

    first commit

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -2,145 +2,144 @@ const Data = [
   {
     id: 1,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2021/12/01/17/28/branch-6838719_960_720.jpg",
-    name: "Vladvictoria",
-    link: "https://pixabay.com/photos/branch-ornaments-lights-tree-6838719/",
+      "https://cdn.pixabay.com/photo/2021/07/20/18/19/bald-eagle-6481346_960_720.jpg",
+    name: "anrita1705",
+    link: "https://pixabay.com/photos/bald-eagle-bird-raptor-bird-of-prey-6481346/",
     size: "small",
   },
   {
     id: 2,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2021/12/05/21/39/christmas-balls-6848782_960_720.jpg",
-    name: "Mammiya",
-    link: "https://pixabay.com/photos/christmas-balls-christmas-advent-6848782/",
+      "https://cdn.pixabay.com/photo/2017/08/02/09/40/sunset-2570443_960_720.jpg",
+    name: "jplenio",
+    link: "https://pixabay.com/photos/sunset-clouds-heaven-view-birds-2570443/",
     size: "medium",
   },
   {
     id: 3,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/07/29/03/58/child-5446839_960_720.jpg",
-    name: "Zhangliams",
-    link: "https://pixabay.com/photos/child-boy-run-alley-street-plants-5446839/",
+      "https://cdn.pixabay.com/photo/2017/12/11/22/42/peacock-feathers-3013486_960_720.jpg",
+    name: "Alexas_Fotos",
+    link: "https://pixabay.com/photos/peacock-feathers-feathers-plumage-3013486/",
     size: "large",
   },
   {
     id: 4,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2016/11/14/09/14/cat-1822979_960_720.jpg",
-    name: "Mshyun",
-    link: "https://pixabay.com/photos/cat-christmas-tree-ornaments-1822979/",
+      "https://cdn.pixabay.com/photo/2013/06/08/00/41/swan-122983_960_720.jpg",
+    name: "Holgi",
+    link: "https://pixabay.com/photos/swan-bird-waterfowl-black-swan-122983/",
     size: "medium",
   },
   {
     id: 5,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2021/11/02/10/46/lemur-6762935_960_720.jpg",
-    name: "Jansheldan",
-    link: "https://pixabay.com/photos/lemur-animal-wilderness-nature-6762935/",
+      "https://cdn.pixabay.com/photo/2017/08/31/05/36/buildings-2699520_960_720.jpg",
+    name: "igormattio",
+    link: "https://pixabay.com/photos/buildings-city-illuminated-2699520/",
     size: "large",
   },
   {
     id: 6,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2021/10/13/07/43/couple-6705694_960_720.jpg",
-    name: "Jupilu",
-    link: "https://pixabay.com/photos/couple-sundown-field-sunset-6705694/",
+      "https://cdn.pixabay.com/photo/2016/07/25/20/11/bird-1541297_960_720.jpg",
+    name: "dennisflarsen",
+    link: "https://pixabay.com/photos/bird-eggs-nest-blue-eggs-wildlife-1541297/",
     size: "small",
   },
   {
     id: 7,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_960_720.jpg",
-    name: "Stergo",
-    link: "https://pixabay.com/photos/fantasy-butterflies-mushrooms-2049567/",
+      "https://cdn.pixabay.com/photo/2016/11/21/16/36/cologne-cathedral-1846338_960_720.jpg",
+    name: "gerdrohsdesign",
+    link: "https://pixabay.com/photos/cologne-cathedral-hohenzollern-bridge-1846338/",
     size: "medium",
   },
   {
     id: 8,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_960_720.jpg",
-    name: "Homecare",
-    link: "https://pixabay.com/photos/bird-fluttering-berries-wings-tit-1045954/",
+      "https://cdn.pixabay.com/photo/2020/02/02/17/24/travel-4813658_960_720.jpg",
+    name: "shalender",
+    link: "https://pixabay.com/photos/travel-delhi-gate-culture-stone-4813658/",
     size: "large",
   },
   {
     id: 9,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2021/11/26/20/44/lantern-6826687_960_720.jpg",
-    name: "Lasrs Nissen",
-    link: "https://pixabay.com/photos/lantern-sea-sky-beach-water-6826687/",
+      "https://cdn.pixabay.com/photo/2017/05/30/21/37/brihadishvara-2358280_960_720.jpg",
+    name:"lapping",
+    link: "https://pixabay.com/photos/brihadishvara-temple-thanjavur-2358280/",
     size: "medium",
   },
   {
     id: 10,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2021/11/30/17/06/tree-6835828_960_720.jpg",
-    name: "Jell Wllington",
-    link: "https://pixabay.com/photos/tree-snow-winter-christmas-branch-6835828/",
+      "https://cdn.pixabay.com/photo/2020/01/03/17/18/architecture-4738647_960_720.jpg",
+    name: "shalender",
+    link: "https://pixabay.com/photos/architecture-indian-worship-lake-4738647/",
     size: "medium",
   },
   {
     id: 11,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg",
-    name: "Chraecker",
-    link: "https://pixabay.com/photos/owl-bird-animal-bird-of-prey-50267/",
+    imgSrc:"https://cdn.pixabay.com/photo/2015/11/18/13/13/diwali-1049141_960_720.jpg",
+    name: "kamodayz",
+    link: "https://pixabay.com/photos/diwali-deepawali-celebration-1049141/",
     size: "small",
   },
   {
     id: 12,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2017/06/07/10/47/elephant-2380009_960_720.jpg",
+      "https://cdn.pixabay.com/photo/2013/10/12/19/38/rice-194768_960_720.jpg",
     name: "Comfreak",
-    link: "https://pixabay.com/photos/elephant-watering-hole-baby-elephant-2380009/",
+    link: "https://pixabay.com/photos/rice-fields-crops-paddy-greenery-194768/",
     size: "medium",
   },
   {
     id: 13,
     imgSrc:
       "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
-    name: "Sasinet",
+    name: "Sarangib",
     link: "https://pixabay.com/photos/woman-kid-rain-leaf-umbrella-1807533/",
     size: "large",
   },
   {
     id: 14,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2019/08/19/07/45/dog-4415649_960_720.jpg",
-    name: "Hauad",
-    link: "https://pixabay.com/photos/dog-corgi-pets-cute-rain-4415649/",
+      "https://cdn.pixabay.com/photo/2014/04/24/11/28/coconuts-331262_960_720.jpg",
+    name: "vijayanarasimha",
+    link: "https://pixabay.com/photos/coconuts-fruit-coco-green-seed-331262/",
     size: "medium",
   },
   {
     id: 15,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014619_960_720.jpg",
-    name: "Chau min",
-    link: "https://pixabay.com/photos/japan-osaka-night-asia-landmark-2014619/",
+      "https://cdn.pixabay.com/photo/2020/04/08/13/24/sunset-5017360_960_720.jpg",
+    name: "amitthakkar46",
+    link: "https://pixabay.com/photos/sunset-urban-city-travel-5017360/",
     size: "large",
   },
   {
     id: 16,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2015/10/22/17/45/leaf-1001679_960_720.jpg",
-    name: "Steven",
-    link: "https://pixabay.com/photos/leaf-green-foliage-green-leaves-1001679/",
+      "https://cdn.pixabay.com/photo/2013/04/23/12/34/color-106692_960_720.jpg",
+    name: "Erikamuth",
+    link: "https://pixabay.com/photos/to-dye-chalk-india-colorful-color-106692/",
     size: "medium",
   },
   {
     id: 17,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2018/05/16/22/27/rose-3407234_960_720.jpg",
-    name: "Rosee",
-    link: "https://pixabay.com/photos/rose-red-drops-rain-macro-nature-3407234/",
+      "https://cdn.pixabay.com/photo/2020/06/02/10/44/mumbai-5250402_960_720.jpg",
+    name: "ameeq",
+    link: "https://pixabay.com/photos/mumbai-slums-poverty-poor-ghetto-5250402/",
     size: "medium",
   },
   {
     id: 18,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2018/01/29/19/00/park-3116883_960_720.jpg",
-    name: "Arcoin",
-    link: "https://pixabay.com/photos/park-bench-night-evening-fog-mist-3116883/",
+      "https://cdn.pixabay.com/photo/2020/04/25/16/37/tractor-5091577_960_720.jpg",
+    name: "dendoktoor",
+    link: "https://pixabay.com/photos/tractor-wallpaper-farmers-farm-5091577/",
     size: "large",
   },
   {
@@ -170,9 +169,10 @@ const Data = [
   {
     id: 22,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2016/11/29/11/12/rain-1869119_960_720.jpg",
-    name: "Pexel",
-    link: "https://pixabay.com/photos/rain-woman-brunette-portrait-1869119/",
+      "https://cdn.pixabay.com/photo/2019/03/21/20/16/india-4071840_960_720.jpg",
+    
+    name: "travelphotographer",
+    link: "https://pixabay.com/photos/india-shop-seller-man-shopping-4071840/",
     size: "medium",
   },
   {
@@ -186,9 +186,9 @@ const Data = [
   {
     id: 24,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2019/01/25/11/18/girl-3954232_960_720.jpg",
-    name: "Pexel",
-    link: "https://pixabay.com/photos/girl-rain-femininity-beauty-paris-3954232/",
+      "https://cdn.pixabay.com/photo/2018/12/04/16/50/pakora-3856054_960_720.jpg",
+    name: "ArtificialOG",
+    link: "https://pixabay.com/photos/pakora-indian-food-indian-kitchen-3856054/",
     size: "large",
   },
   {
@@ -202,9 +202,9 @@ const Data = [
   {
     id: 26,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/08/30/09/28/buildings-5528981_960_720.jpg",
-    name: "China Town",
-    link: "https://pixabay.com/photos/buildings-houses-street-city-rain-5528981/",
+      "https://cdn.pixabay.com/photo/2022/01/27/07/34/manchurian-6971355_960_720.jpg",
+    name: "Anilsharma26",
+    link: "https://pixabay.com/photos/manchurian-indian-food-food-6971355/",
     size: "small",
   },
   {
@@ -218,9 +218,9 @@ const Data = [
   {
     id: 28,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/04/02/23/01/rain-4996916_960_720.jpg",
-    name: "Pyou",
-    link: "https://pixabay.com/photos/rain-street-tables-cafe-flowers-4996916/",
+      "https://cdn.pixabay.com/photo/2018/06/11/06/58/hope-3467922_960_720.jpg",
+    name: "nandhukumar",
+    link: "https://pixabay.com/photos/hope-kerala-food-rice-outdoor-3467922/",
     size: "small",
   },
   {
@@ -234,10 +234,34 @@ const Data = [
   {
     id: 30,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg",
-    name: "cat",
-    link: "https://pixabay.com/photos/cat-kitten-pet-kitty-young-cat-551554/",
+      "https://cdn.pixabay.com/photo/2020/08/16/07/55/temple-5492096_960_720.jpg",
+    name: "gauravfkumar",
+    link: "https://pixabay.com/photos/temple-building-mountain-forest-5492096/",
     size: "large",
+  },
+  {
+    id: 31,
+    imgSrc:
+      "https://cdn.pixabay.com/photo/2020/02/02/17/24/travel-4813658_960_720.jpg",
+    name: "shalender",
+    link: "https://pixabay.com/photos/travel-delhi-gate-culture-stone-4813658/",
+    size: "large",
+  },
+  {
+    id: 32,
+    imgSrc:
+      "https://cdn.pixabay.com/photo/2017/08/02/09/40/sunset-2570443_960_720.jpg",
+    name: "jplenio",
+    link: "https://pixabay.com/photos/sunset-clouds-heaven-view-birds-2570443/",
+    size: "medium",
+  },
+  {
+    id: 33,
+    imgSrc:
+      "https://cdn.pixabay.com/photo/2014/04/24/11/28/coconuts-331262_960_720.jpg",
+    name: "vijayanarasimha",
+    link: "https://pixabay.com/photos/coconuts-fruit-coco-green-seed-331262/",
+    size: "medium",
   },
 ];
 
